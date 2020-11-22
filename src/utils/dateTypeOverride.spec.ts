5c9b996f83b4adb44f5980240fd1623a8309e1df
Nico Vogel <nico-vogel97@outlook.de>
2020-11-22 21:52:38 +0100

    create dateTypeOverride test file

---
+++ src/utils/dateTypeOverride.spec.ts
@@ -0,0 +1,26 @@
+import type { Model } from '../client/interfaces/Model';
+
+describe('dateTypeOverride', () => {
+    it('', async () => {
+        const models: Model[] = [
+            {
+                export: 'interface',
+                name: 'MyModel',
+                type: 'MyModel',
+                base: 'MyModel',
+                template: null,
+                link: null,
+                description: null,
+                isDefinition: true,
+                isReadOnly: false,
+                isRequired: false,
+                isNullable: false,
+                imports: [],
+                extends: [],
+                enum: [],
+                enums: [],
+                properties: [],
+            },
+        ];
+    });
+});
