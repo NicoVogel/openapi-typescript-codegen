081c3a0442fb4ba2292a3e60fc62754370b4b254
Nico Vogel <nico-vogel97@outlook.de>
2020-12-03 17:02:36 +0100

    TODO squash later

--- src/utils/dateTypeOverride.spec.ts
+++ src/utils/dateTypeOverride.spec.ts
@@ -1,26 +1,28 @@
 import type { Model } from '../client/interfaces/Model';
 
+const models: Model[] = [
+    {
+        export: 'interface',
+        name: 'MyModel',
+        type: 'MyModel',
+        base: 'MyModel',
+        template: null,
+        link: null,
+        description: null,
+        isDefinition: true,
+        isReadOnly: false,
+        isRequired: false,
+        isNullable: false,
+        imports: [],
+        extends: [],
+        enum: [],
+        enums: [],
+        properties: [],
+    },
+];
+
 describe('dateTypeOverride', () => {
-    it('', async () => {
-        const models: Model[] = [
-            {
-                export: 'interface',
-                name: 'MyModel',
-                type: 'MyModel',
-                base: 'MyModel',
-                template: null,
-                link: null,
-                description: null,
-                isDefinition: true,
-                isReadOnly: false,
-                isRequired: false,
-                isNullable: false,
-                imports: [],
-                extends: [],
-                enum: [],
-                enums: [],
-                properties: [],
-            },
-        ];
+    it('sucess', async () => {
+
     });
 });
