67018704857e6cef669dea2a6dee20194ed4e4e2
Nico Vogel <nico-vogel97@outlook.de>
2020-12-07 21:10:47 +0100

    implement test that check if the output file does in fact contain the correct type

--- test/index.spec.js
+++ test/index.spec.js
@@ -47,7 +47,7 @@ describe('v3', () => {
 });
 
 describe('v3 datetype', () => {
-    it('should generate files and apply the Date type to interfaces with the string format "date" or "date-time"', async () => {
+    it('should generate files and apply the Date type to interfaces with the string format "date-time"', async () => {
         await OpenAPI.generate({
             input: './test/spec/v3_datetype.yaml',
             output: './test/generated/v3_datetype/',
@@ -64,6 +64,22 @@ describe('v3 datetype', () => {
         glob.sync('./test/generated/v3_datetype/**/*.ts').forEach(file => {
             const content = fs.readFileSync(file, 'utf8').toString();
             expect(content).toMatchSnapshot(file);
+        });
+
+        glob.sync('./test/generated/v3_datetype/models/*.ts').forEach(file => {
+            const content = fs.readFileSync(file, 'utf8').toString();
+            const getNameAndContentPattern = /export (?:interface|type) (\w+)([\s\S]+)*?\}/;
+            const regexGroup = getNameAndContentPattern.exec(content);
+            const interfaceName = regexGroup[1] || '';
+            const interfaceContent = regexGroup[2] || '';
+            const attribute = interfaceContent
+                .split('\n')
+                .map(att => att.trim())
+                .find(att => att.startsWith('dateTime'));
+            if (attribute) {
+                console.log(`The interface ${interfaceName} with an attribute named 'dateTime' should have the data type of 'Date'`)
+                expect(attribute).toMatch(/Date/);
+            }
         });
     });
 });
