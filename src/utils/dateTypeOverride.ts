86875c2b369625894e4ced2b6f6427baafdd261d
Nico Vogel <nico-vogel97@outlook.de>
2020-12-19 17:12:34 +0100

    add RequiredFields<T> type in dateTypeOverride.ts

--- src/utils/dateTypeOverride.ts
+++ src/utils/dateTypeOverride.ts
@@ -1,11 +1,12 @@
 import { Model } from '../client/interfaces/Model.d';
 
 const formatDate = ['date-time'];
+export type RequiredFields<T> = Pick<Model, 'export' | 'format' | 'base'> & { properties: RequiredFields<T>[]; link: RequiredFields<T> | null } & T;
 /**
  * Change Model.base if it is a string and has the format 'date-time'
  * @param models Array of Models
  */
-export function dateTypeOverride(models: Model[]): Model[] {
+export function dateTypeOverride<T>(models: RequiredFields<T>[]): RequiredFields<T>[] {
     return models.map(model => {
         if (model.export === 'interface') {
             return { ...model, properties: dateTypeOverride(model.properties) };
